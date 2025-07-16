72f950813618293bb0c29a09e3c8102e3186771e
David Nienge <davidnienge@gmail.com>
2025-07-16 18:24:11 +0100

    Update badge.tsx

--- components/badge.tsx
+++ components/badge.tsx
@@ -8,15 +8,15 @@ export function Badges() {
     <Container>
       <div className="mt-12 w-full flex flex-col md:flex-row items-center justify-between gap-6 mx-auto">
         <div className="flex w-full flex-wrap gap-2">
-          <Badge className="p-2 px-3 bg-myBlack">Computer Programming</Badge>
+          <Badge className="p-2 px-3 bg-myBlack">Your Child will build:</Badge>
           <Badge className="p-2 px-3 bg-myPrimary" variant="secondary">
-            HTML
+            Apps
           </Badge>
           <Badge className="p-2 px-3" variant="destructive">
-            CSS
+            Games
           </Badge>
           <Badge className="p-2 px-3 bg-white" variant="outline">
-            JavaScript
+            Websites
           </Badge>
         </div>
         <div className="flex w-full flex-wrap gap-2">
